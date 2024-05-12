fe6dce07fd9826156f35a0506841449f8bf423de
xavierchanth <xchanthavong@gmail.com>
2024-05-11 21:37:21 -0400

    xavierchanth custom keys

---
+++ keyboards/nuphy/air75_v2/ansi/keymaps/xavierchanth/config.h
@@ -0,0 +1,8 @@
+#define PERMISSIVE_HOLD
+/* #define PERMISSIVE_HOLD_PER_KEY */
+/* #define HOLD_ON_OTHER_KEY_PRESS */
+/* #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY */
+/* #define IGNORE_MOD_TAP_INTERRUPT */
+
+#define TAPPING_TERM 200
+/* #define QUICK_TAP_TERM */
