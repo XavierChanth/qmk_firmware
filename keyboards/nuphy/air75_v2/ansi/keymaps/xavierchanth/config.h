d3c68f29c3c2a637b791040770025ea432b4c235
xavierchanth <xchanthavong@gmail.com>
2024-05-12 09:23:26 -0400

    permissive hold really does suck... maybe if there was permissive hold grace period

--- keyboards/nuphy/air75_v2/ansi/keymaps/xavierchanth/config.h
+++ keyboards/nuphy/air75_v2/ansi/keymaps/xavierchanth/config.h
@@ -1,8 +1,7 @@
-#define PERMISSIVE_HOLD
+#define TAPPING_TERM 250
+#define QUICK_TAP_TERM 125
+/* #define PERMISSIVE_HOLD */
 /* #define PERMISSIVE_HOLD_PER_KEY */
 /* #define HOLD_ON_OTHER_KEY_PRESS */
 /* #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY */
 /* #define IGNORE_MOD_TAP_INTERRUPT */
-
-#define TAPPING_TERM 150
-/* #define QUICK_TAP_TERM */
